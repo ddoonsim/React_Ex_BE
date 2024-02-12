3933748191f6606e3282a6b0f22cd392c6942f73
ddoonsim <2sujg97@naver.com>
2024-02-12 23:34:30 +0900

    백엔드 기초 작업,,

---
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.choongang.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.build();
+    }
+
+    /**
+     * BCrypt 인코더 사용을 위한 설정
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/org/choongang/models/member/MemberInfoService.java
@@ -0,0 +1,40 @@
+package org.choongang.models.member;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.commons.constants.MemberType;
+import org.choongang.entities.Member;
+import org.choongang.repositories.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class MemberInfoService implements UserDetailsService {
+
+    private final MemberRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Member member = repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        // 사용자의 권한
+        MemberType type = Objects.requireNonNullElse(member.getType(), MemberType.USER);
+        List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority(type.name()));
+
+        return MemberInfo.builder()
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .authorities(authorities)
+                .member(member)
+                .build();
+    }
+}
